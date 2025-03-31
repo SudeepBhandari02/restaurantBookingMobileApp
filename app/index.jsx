3ba5edfa5498966fddc47d858f264484690eb550
SudeepBhandari02 <sudeepdbhandari@gmail.com>
2025-03-31 23:37:44 +0530

    Has set all the required configurations and ready to develope

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 items-center justify-start pt-4 bg-gray-100 dark:bg-gray-500"
+    >
+      <Text className=" text-4xl text-cyan-500 tracking-wider font-bold">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
