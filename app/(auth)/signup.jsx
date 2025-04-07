912e73b0d15ab20c6dbba310901639f839973a43
SudeepBhandari02 <sudeepdbhandari@gmail.com>
2025-04-08 00:59:38 +0530

    added routes for sign in and signup

---
+++ app/(auth)/signup.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const signup = () => {
+  return (
+    <View className={"items-center justify-center"}>
+      <Text className={"text-4xl color-cyan-600"}>signup</Text>
+    </View>
+  )
+}
+
+export default signup;
