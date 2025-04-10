ae3dce8640fc10a1928990b7ac7885a7fe55b54e
SudeepBhandari02 <sudeepdbhandari@gmail.com>
2025-04-10 22:40:00 +0530

    Created the signin page

--- app/(auth)/signin.jsx
+++ app/(auth)/signin.jsx
@@ -1,12 +1,143 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+    View,
+    Text,
+    ScrollView,
+    Image,
+    TouchableOpacity,
+    TextInput,
+    Alert,
+    TouchableWithoutFeedback
+} from "react-native";
+import React, {useState} from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import logo from "../../assets/images/dinetimelogo.png";
+import colors from "../../assets/themeColors";
+import frame from "../../assets/images/Frame.png";
+import { Formik } from "formik";
+import validationSchema from "../../validation/validationSchema";
+import { router } from "expo-router";
+import Entypo from '@expo/vector-icons/Entypo';
 
-const signin = () => {
-  return (
-    <View>
-      <Text>signin</Text>
-    </View>
-  )
-}
+const signup = () => {
+    const [iconName, setIconName] = useState("eye");
+    const [passwordVisibility, setPasswordVisibility] = useState(true);
+    const handleSignin = () => {
+        Alert.alert("Account Created Sucessfully!");
+    };
+    const toggleIconName = ()=>{
+        if(iconName === "eye"){
+            setIconName("eye-with-line");
+            setPasswordVisibility(false);
+        }else{
+            setIconName("eye");
+            setPasswordVisibility(true);
+        }
+    }
 
-export default signin
+    return (
+        <SafeAreaView style={{ backgroundColor: colors.SECONDARY }}>
+            <ScrollView
+                style={{ height: "100%" }}
+                contentContainerStyle={{ justifyContent: "center" }}
+            >
+                <View className={"flex items-center justify-center"}>
+                    <Image
+                        source={logo}
+                        style={{
+                            width: 400,
+                            height: 200,
+                        }}
+                    />
+
+                    <View className={"w-3/4 flex  items-center justify-center"}>
+                        <Text className="text-lg text-center text-white  font-semibold mb-10">
+                            Let's get you started
+                        </Text>
+
+                        <Formik
+                            initialValues={{ email: "", password: "" }}
+                            validationSchema={validationSchema}
+                            onSubmit={handleSignin}
+                        >
+                            {({
+                                  handleChange,
+                                  handleBlur,
+                                  handleSubmit,
+                                  values,
+                                  errors,
+                                  touched,
+                              }) => (
+                                <View className="w-full">
+                                    <Text className="text-[#f49b33] mt-4 mb-2">Email</Text>
+                                    <TextInput
+                                        className="h-16 border border-white text-white rounded px-2"
+                                        keyboardType="email-address"
+                                        onChangeText={handleChange("email")}
+                                        value={values.email}
+                                        onBlur={handleBlur("email")}
+                                    />
+
+                                    {touched.email && errors.email && (
+                                        <Text className="text-red-500 text-xs mb-2">
+                                            {errors.email}
+                                        </Text>
+                                    )}
+                                    <Text className="text-[#f49b33] mt-4 mb-2">Password</Text>
+                                    <View className="h-16 border border-white text-white rounded px-2 flex flex-row justify-between items-center">
+                                        <TextInput className={"flex-1 text-white"}
+                                                   secureTextEntry={passwordVisibility}
+                                                   onChangeText={handleChange("password")}
+                                                   value={values.password}
+                                                   onBlur={handleBlur("password")}
+                                        />
+                                        <TouchableWithoutFeedback className={"p-2"} onPress={toggleIconName}>
+                                            <Entypo name={iconName} size={28} color={colors.PRIMARY} />
+                                        </TouchableWithoutFeedback>
+                                    </View>
+
+                                    {touched.password && errors.password && (
+                                        <Text className="text-red-500 text-xs mb-2">
+                                            {errors.password}
+                                        </Text>
+                                    )}
+
+                                    <TouchableOpacity
+                                        onPress={handleSubmit}
+                                        className="p-2 my-2 bg-[#f49b33]  text-black rounded-lg mt-10"
+                                    >
+                                        <Text className="text-lg font-semibold text-center">
+                                            Sign In
+                                        </Text>
+                                    </TouchableOpacity>
+                                </View>
+                            )}
+                        </Formik>
+                    </View>
+
+                    <View className={"flex flex-row items-center justify-center my-3"}>
+                        <Text
+                            className={"text-center mr-2"}
+                            style={{ color: colors.dark.text }}
+                        >
+                            New user?
+                        </Text>
+                        <TouchableOpacity onPress={() => router.push("/signup")}>
+                            <Text
+                                className={"font-semibold text-xl"}
+                                style={{ color: colors.PRIMARY }}
+                            >
+                                Sign Up
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    <View className={"flex-1 mt-10 "}>
+                        <Image source={frame} resizeMode={"contain"} />
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
+};
+
+export default signup;
--- app/(auth)/signup.jsx
+++ app/(auth)/signup.jsx
@@ -1,98 +1,143 @@
-import {View, Text, ScrollView, Image, TouchableOpacity, TextInput, Alert} from 'react-native'
-import React from 'react'
-import {SafeAreaView} from "react-native-safe-area-context";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  Alert,
+  TouchableWithoutFeedback,
+} from "react-native";
+import React, {useState} from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 import logo from "../../assets/images/dinetimelogo.png";
 import colors from "../../assets/themeColors";
 import frame from "../../assets/images/Frame.png";
-import {Formik} from "formik";
+import { Formik } from "formik";
 import validationSchema from "../../validation/validationSchema";
+import { router } from "expo-router";
+import Entypo from '@expo/vector-icons/Entypo';
 
 const signup = () => {
-    const handleSubmit = ()=>{
-        Alert.alert("Account Created Sucessfully!");
+  const [iconName, setIconName] = useState("eye");
+  const [passwordVisibility, setPasswordVisibility] = useState(true);
+  const handleSignUp = () => {
+    Alert.alert("Account Created Sucessfully!");
+  };
+  const toggleIconName = ()=>{
+    if(iconName === "eye"){
+      setIconName("eye-with-line");
+      setPasswordVisibility(false);
+    }else{
+      setIconName("eye");
+      setPasswordVisibility(true);
     }
+  }
 
   return (
-    <SafeAreaView style={{backgroundColor:colors.SECONDARY}}>
-        <ScrollView style={{height:'100%'}} contentContainerStyle={{justifyContent:'center'}}>
-            <View className={"flex items-center justify-center"}>
-                <Image source={logo} style={{
-                    width: 400,height:200
-                }}/>
+    <SafeAreaView style={{ backgroundColor: colors.SECONDARY }}>
+      <ScrollView
+        style={{ height: "100%" }}
+        contentContainerStyle={{ justifyContent: "center" }}
+      >
+        <View className={"flex items-center justify-center"}>
+          <Image
+            source={logo}
+            style={{
+              width: 400,
+              height: 200,
+            }}
+          />
 
-                <View className={"w-3/4 flex  items-center justify-center"}>
-                    <Text className="text-lg text-center text-white  font-semibold mb-10">
-                        Let's get you started
+          <View className={"w-3/4 flex  items-center justify-center"}>
+            <Text className="text-lg text-center text-white  font-semibold mb-10">
+              Let's get you started
+            </Text>
+
+            <Formik
+              initialValues={{ email: "", password: "" }}
+              validationSchema={validationSchema}
+              onSubmit={handleSignUp}
+            >
+              {({
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                values,
+                errors,
+                touched,
+              }) => (
+                <View className="w-full">
+                  <Text className="text-[#f49b33] mt-4 mb-2">Email</Text>
+                  <TextInput
+                    className="h-16 border border-white text-white rounded px-2"
+                    keyboardType="email-address"
+                    onChangeText={handleChange("email")}
+                    value={values.email}
+                    onBlur={handleBlur("email")}
+                  />
+
+                  {touched.email && errors.email && (
+                    <Text className="text-red-500 text-xs mb-2">
+                      {errors.email}
                     </Text>
+                  )}
+                  <Text className="text-[#f49b33] mt-4 mb-2">Password</Text>
+                  <View className="h-16 border border-white text-white rounded px-2 flex flex-row justify-between items-center">
+                    <TextInput className={"flex-1 text-white"}
+                      secureTextEntry={passwordVisibility}
+                      onChangeText={handleChange("password")}
+                      value={values.password}
+                      onBlur={handleBlur("password")}
+                    />
+                    <TouchableWithoutFeedback className={"p-2"} onPress={toggleIconName}>
+                      <Entypo name={iconName} size={28} color={colors.PRIMARY} />
+                    </TouchableWithoutFeedback>
+                  </View>
 
-                    <Formik
-                        initialValues={{ email: "", password: "" }}
-                        validationSchema={validationSchema}
-                        onSubmit={handleSubmit}
-                    >
-                        {({
-                              handleChange,
-                              handleBlur,
-                              handleSubmit,
-                              values,
-                              errors,
-                              touched,
-                          }) => (
-                            <View className="w-full">
-                                <Text className="text-[#f49b33] mt-4 mb-2">Email</Text>
-                                <TextInput
-                                    className="h-16 border border-white text-white rounded px-2"
-                                    keyboardType="email-address"
-                                    onChangeText={handleChange("email")}
-                                    value={values.email}
-                                    onBlur={handleBlur("email")}
-                                />
+                  {touched.password && errors.password && (
+                    <Text className="text-red-500 text-xs mb-2">
+                      {errors.password}
+                    </Text>
+                  )}
 
-                                {touched.email && errors.email && (
-                                    <Text className="text-red-500 text-xs mb-2">
-                                        {errors.email}
-                                    </Text>
-                                )}
-                                <Text className="text-[#f49b33] mt-4 mb-2">Password</Text>
-                                <TextInput
-                                    className="h-16 border border-white text-white rounded px-2"
-                                    secureTextEntry
-                                    onChangeText={handleChange("password")}
-                                    value={values.password}
-                                    onBlur={handleBlur("password")}
-                                />
-
-                                {touched.password && errors.password && (
-                                    <Text className="text-red-500 text-xs mb-2">
-                                        {errors.password}
-                                    </Text>
-                                )}
-
-                                <TouchableOpacity
-                                    onPress={handleSubmit}
-                                    className="p-2 my-2 bg-[#f49b33]  text-black rounded-lg mt-10"
-                                >
-                                    <Text className="text-lg font-semibold text-center">
-                                        Sign Up
-                                    </Text>
-                                </TouchableOpacity>
-                            </View>
-                        )}
-                    </Formik>
-
+                  <TouchableOpacity
+                    onPress={handleSubmit}
+                    className="p-2 my-2 bg-[#f49b33]  text-black rounded-lg mt-10"
+                  >
+                    <Text className="text-lg font-semibold text-center">
+                      Sign Up
+                    </Text>
+                  </TouchableOpacity>
                 </View>
+              )}
+            </Formik>
+          </View>
 
+          <View className={"flex flex-row items-center justify-center my-3"}>
+            <Text
+              className={"text-center mr-2"}
+              style={{ color: colors.dark.text }}
+            >
+              Already a user?
+            </Text>
+            <TouchableOpacity onPress={() => router.push("/signin")}>
+              <Text
+                className={"font-semibold text-xl"}
+                style={{ color: colors.PRIMARY }}
+              >
+                Login
+              </Text>
+            </TouchableOpacity>
+          </View>
 
-
-
-                <View className={"flex-1 mt-20 "}>
-                    <Image source={frame} resizeMode={"contain"}/>
-                </View>
-
-            </View>
-        </ScrollView>
+          <View className={"flex-1 mt-10 "}>
+            <Image source={frame} resizeMode={"contain"} />
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 export default signup;
